6a047e6c6e999c40fac4f705eb3d88e588adf918
Dan Scannell <mystik3eb@gmail.com>
2017-08-22 23:02:04 -0700

    test for getting total lead count

--- src/analyzeGuestCards.spec.js
+++ src/analyzeGuestCards.spec.js
@@ -2,33 +2,9 @@ const analyzer = require('./analyzeGuestCards');
 
 it('should take an array of cards and return a list of unique marketing sources', () => {
     const testGuestCards = [{
-        id: 1819908,
-        first_seen: '2014-04-04',
-        expected_move_in: null,
-        shown_unit: null,
-        agent_id: 3519,
-        marketing_source: 'Rentlix.com',
-        application_submitted: null,
-        application_approved: null,
-        application_denied: null,
-        application_canceled: null,
-        lease_signed: null,
-        resident_rent: null,
-        unit_name: null
+        marketing_source: 'Rentlix.com'
     }, {
-        id: 1819911,
-        first_seen: '2014-04-07',
-        expected_move_in: '2014-05-01',
-        shown_unit: null,
-        agent_id: 3519,
-        marketing_source: 'Craigslist.com',
-        application_submitted: null,
-        application_approved: null,
-        application_denied: null,
-        application_canceled: null,
-        lease_signed: null,
-        resident_rent: null,
-        unit_name: null
+        marketing_source: 'Craigslist.com'
     }];
 
     const marketingSources = analyzer.getMarketingSources(testGuestCards);
@@ -48,8 +24,25 @@ it('should take array of marketing sources and return objects', () => {
     expect(marketingSourceObjects[1].name).toBe('Craigslist.com');
 });
 
-it.skip('should find the total number of leads for every marketing source in a guest card array', () => {
+it('should find the total number of leads for every marketing source in a guest card array', () => {
+    const testGuestCards = [{
+        marketing_source: 'Rentlix.com'
+    }, {
+        marketing_source: 'Craigslist.com'
+    }, {
+        marketing_source: 'Rentlix.com'
+    }, {
+        marketing_source: 'Craigslist.com'
+    }, {
+        marketing_source: 'Craigslist.com'
+    }];
 
+    const marketingSourcesNames = analyzer.getMarketingSources(testGuestCards);
+    let marketingSourceObjects = analyzer.makeMarketingSourceObjects(marketingSourcesNames);
+    marketingSourceObjects = analyzer.getTotalLeases(marketingSourceObjects, testGuestCards);
+
+    expect(marketingSourceObjects['Rentlix.com'].totalLeads).toBe(2);
+    expect(marketingSourceObjects['Craigslist.com'].totalLeads).toBe(3);
 });
 
 it.skip('should take an array of cards and return another array of analyzes objects', () => {
